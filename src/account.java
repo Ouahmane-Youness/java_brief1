99bd0ce9828e6c235755374af77e8fcd55d48842
youness <youness.ouahmane4@gmail.com>
2025-09-17 21:34:55 +0100

    add credit and debit methods

--- src/account.java
+++ src/account.java
@@ -10,5 +10,25 @@ class Account{
         this.accountType = accountType;
     }
 
+    public void creditAccount(double total) throws Exception
+    {
+        if(total < 0)
+        {
+            throw new Exception("total has to be positive");
+        }
+        this.sold += total;
+    }
 
+    public void debitAccount(double total) throws Exception
+    {
+        if(total < 0)
+        {
+            throw new Exception("total has to be positive");
+        }
+        if(total > sold)
+        {
+            throw new Exception("sold is insufficient");
+        }
+        sold -= total;
+    }
 }
