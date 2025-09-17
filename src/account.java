2ffbea3b3c9f478cca41718b30272bd1d611984a
youness <youness.ouahmane4@gmail.com>
2025-09-17 21:21:56 +0100

    account class and constructor

---
+++ src/account.java
@@ -0,0 +1,14 @@
+class Account{
+    private String accountNumber;
+    private double sold;
+    private String accountType;
+
+    public Account(String accountNumber, double sold, String accountType)
+    {
+        this.accountNumber = accountNumber;
+        this.sold = sold;
+        this.accountType = accountType;
+    }
+
+
+}
